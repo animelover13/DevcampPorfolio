0d6ae763498c32ba76b5f26d857a9d004044eda7
slcwestdevcamp <slcwest@devcamp.com>
2017-06-19 01:25:25 +0000

    Inigarted skills via model generator

--- my_api/db/schema.rb
+++ my_api/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614171518) do
+ActiveRecord::Schema.define(version: 20170616194756) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
